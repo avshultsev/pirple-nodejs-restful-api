2181a033ef8bc2c47605c484fc974a106d71efcd
avshultsev <splendide.mendax.gamer.z@gmail.com>
2021-05-20 22:26:35 +0300

    Implemented POST req for "/users"

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
-const createEnvironment = (port = 3000, httpsPort = 3001, envName = 'staging') => {
-  return { port, httpsPort, envName };
+const createEnvironment = (secret, port = 3000, httpsPort = 3001, envName = 'staging') => {
+  return { port, httpsPort, envName, secret };
 };
 
 const environments = {
-  'staging': createEnvironment(),
-  'production': createEnvironment(5000, 5001, 'production'),
+  'staging': createEnvironment('HTTPsecret'),
+  'production': createEnvironment('HTTPSsecret', 5000, 5001, 'production'),
 };
 
 const { NODE_ENV } = process.env;
--- main.js
+++ main.js
@@ -1,7 +1,8 @@
-const http  = require('http');
-const https = require('https');
-const fs    = require('fs');
-const { port: PORT, httpsPort, envName } = require('./config.js');
+const http   = require('http');
+const https  = require('https');
+const fs     = require('fs');
+const crypto = require('crypto');
+const { port: PORT, httpsPort, envName, secret } = require('./config.js');
 const { createFile, readFile, updateFile, deleteFile } = require('./lib/crud.js');
 
 const receiveArgs = async (req) => {
@@ -12,23 +13,56 @@ const receiveArgs = async (req) => {
 };
 
 const routing = {
-  '/': async () => 'Welcome to the main page!',
-  '/ping': async () => 'I\'m alive!',
-  '/hello': async () => 'Hello there! Nice to meet you!',
+  '/': {
+    get: async () => 'Welcome to the main page!',
+  },
+  '/ping': {
+    get: async () => 'I\'m alive!',
+  },
+  '/hello': {
+    get: async () => 'Hello there! Nice to meet you!',
+  },
+  '/users': {
+    get: async () => {},
+    post: async (payload, statusCode) => {
+      const required = ['firstName', 'lastName', 'phone', 'password', 'tosAgreement'];
+      for (const prop of required) {
+        if (!payload[prop]) return {result: 'Missing required fields!', statusCode: 400};
+      }
+      const { phone, password } = payload;
+      // f. ex. 7, could be any other value
+      if (phone.length < 7) return {result: 'Phone number too short!', statusCode: 400};
+      const data = {...payload};
+      const hashedPassword = crypto.createHmac('sha256', secret).update(password).digest('hex');
+      data.password = hashedPassword;
+      try {
+        const result = await createFile('users', `${phone}.json`, data);
+        return {result, statusCode};
+      } catch (err) {
+        console.log(err);
+        return {result: 'User already exists!', statusCode: 500};
+      }
+    },
+    put: async () => {},
+    delete: async () => {},
+  }
 };
+
+const notFound = (res) => {
+  res.writeHead(404, 'Not Found', {'Content-Type' : 'text/plain'});
+  res.end('Not Found');
+}
 
 const listener = async (req, res) => {
   const { method, url, headers } = req;
   let body = null;
-  if (method === "POST") body = receiveArgs(req);
-  const handler = routing[url];
-  if (!handler) {
-    res.writeHead(404, 'Not Found', {'Content-Type' : 'text/plain'});
-    res.end('Not Found');
-    return;
-  }
-  const result = await handler();
-  res.writeHead(200, {'Content-Type' : 'application/json'});
+  if (method === "POST") body = await receiveArgs(req);
+  const route = routing[url];
+  if (!route) return notFound(res);
+  const handler = route[method.toLowerCase()];
+  if (!handler) return notFound(res);
+  const { result, statusCode } = await handler(body, 200);
+  res.writeHead(statusCode, {'Content-Type' : 'application/json'});
   res.end(JSON.stringify({ result, body }));
 };
 
