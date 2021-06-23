26158a80f3b68a2391a0fccc04b30b4bf0bb5870
avshultsev <splendide.mendax.gamer.z@gmail.com>
2021-06-23 20:44:35 +0300

    handlers objects refactored for simplicity; support of serving the static files added

--- server.js
+++ server.js
@@ -1,10 +1,27 @@
 const http  = require('http');
 const https = require('https');
 const fs    = require('fs');
+const path  = require('path');
 const { port: PORT, httpsPort, envName } = require('./config.js');
 const userHandlers   = require('./lib/userHandlers.js');
 const tokenHandlers  = require('./lib/tokenHandlers.js');
 const checksHandlers = require('./lib/checkHandlers.js');
+
+const MIME_TYPES = {
+  html: 'text/html; charset=UTF-8',
+  js: 'application/javascript; charset=UTF-8',
+  css: 'text/css',
+  png: 'image/png',
+  ico: 'image/x-icon',
+  svg: 'image/svg+xml',
+};
+
+const staticPath = path.join(__dirname, 'public');
+const serveFile = (name) => {
+  const filePath = path.join(staticPath, name);
+  const stream = fs.createReadStream(filePath);
+  return stream;
+};
 
 const receiveArgs = async (req) => {
   const chunks = [];
@@ -17,7 +34,7 @@ const parseQueryParams = ({ url, headers }) => {
   const { host } = headers;
   const port = host.split(':')[1];
   const protocol = port.includes('1') ? 'https' : 'http';
-  const urlObj = new URL(`${protocol}://${host}${url}`);
+  const urlObj = new URL(`${protocol}://${host}${url}`); // ${http}://${localhost:3000}${/api/users?phone=123456789}
   const queryParams = {};
   const endpoint = urlObj.pathname;
   if (urlObj.search === '') return { queryParams, endpoint };
@@ -28,33 +45,15 @@ const parseQueryParams = ({ url, headers }) => {
 };
 
 const routing = {
-  '/': {
-    get: async () => 'Welcome to the main page!',
-  },
   '/ping': {
     get: async () => 'I\'m alive!',
   },
   '/hello': {
     get: async () => 'Hello there! Nice to meet you!',
   },
-  '/users': {
-    get: userHandlers._get,
-    post: userHandlers._post,
-    put: userHandlers._put,
-    delete: userHandlers._delete,
-  },
-  '/tokens': {
-    get: tokenHandlers._get,
-    post: tokenHandlers._post,
-    put: tokenHandlers._put,
-    delete: tokenHandlers._delete,
-  },
-  '/checks': {
-    get: checksHandlers._get,
-    post: checksHandlers._post,
-    put: checksHandlers._put,
-    delete: checksHandlers._delete,
-  },
+  '/api/users' : userHandlers,
+  '/api/tokens': tokenHandlers,
+  '/api/checks': checksHandlers,
 };
 
 const notFound = (res) => {
@@ -63,19 +62,31 @@ const notFound = (res) => {
 }
 
 const listener = async (req, res) => {
-  const { method } = req;
-  const { token } = req.headers;
-  const { queryParams, endpoint } = parseQueryParams(req);
-  let body = null;
-  const route = routing[endpoint];
-  if (!route) return notFound(res);
-  const handler = route[method.toLowerCase()];
-  if (!handler) return notFound(res);
-  const bodyRequired = handler.toString().startsWith('async ({ body');
-  if (bodyRequired) body = await receiveArgs(req);
-  const { result, statusCode } = await handler({ body, queryParams, token });
-  res.writeHead(statusCode, {'Content-Type' : 'application/json'});
-  res.end(JSON.stringify({ result, body }));
+  const { method, url, headers } = req;
+  const { token } = headers;
+  const { queryParams, endpoint } = parseQueryParams({ url, headers });
+  if (url.startsWith('/api')) {
+    let body = null;
+    const route = routing[endpoint];
+    if (!route) return notFound(res);
+    const handler = route[method.toLowerCase()];
+    if (!handler) return notFound(res);
+    const bodyRequired = handler.toString().startsWith('async ({ body');
+    if (bodyRequired) body = await receiveArgs(req);
+    const { result, statusCode } = await handler({ body, queryParams, token });
+    res.writeHead(statusCode, { 'Content-Type': 'application/json' });
+    res.end(JSON.stringify({ result, body }));
+  }
+  const name = url === '/' ? '/index.html' : url; // try 'index.html' (without slash)
+  const fileExt = path.extname(name).substring(1) || 'html';
+  const type = MIME_TYPES[fileExt] || MIME_TYPES.html;
+  const stream = serveFile(name);
+  stream.on('error', () => {
+    res.writeHead(404, 'Not Found!');
+    res.end();
+  });
+  res.writeHead(200, { 'Content-Type': type });
+  stream.pipe(res);
 };
 
 const httpsOptions = {
