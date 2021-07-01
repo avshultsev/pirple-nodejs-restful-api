2a4f12b406e4cf8a1231adde29a1f2639ce1056d
avshultsev <splendide.mendax.gamer.z@gmail.com>
2021-07-01 21:30:41 +0300

    user profile editing and deletion

--- public/js/app.js
+++ public/js/app.js
@@ -2,7 +2,7 @@ const request = async (path = 'http://localhost:3000/', method = 'GET', payload 
   let url = path;
   const queryParams = Object.keys(queryObj).reduce((acc, key, index, arr) => {
     let str = `${key}=${queryObj[key]}`;
-    if (index === arr.length - 1) str += '&';
+    if (index < arr.length - 1) str += '&';
     return acc += str;
   }, '');
   if (queryParams) url += '?' + queryParams;
@@ -31,7 +31,7 @@ const displayError = (div, msg) => {
 
 const setLoggedInClass = (isLoggedIn) => {
   const body = document.querySelector('body');
-  isLoggedIn 
+  isLoggedIn
     ? body.classList.add('loggedIn')
     : body.classList.remove('loggedIn');
 };
@@ -56,7 +56,7 @@ const updateToken = (tokenStr) => {
 
 const periodicTokenRenew = () => {
   const interval = setInterval(() => {
-    const tokenStr = localStorage.getItem('token');
+    const tokenStr = getToken();
     if (!tokenStr) return clearInterval(interval);
     updateToken(tokenStr);
   }, 1000 * 60);
@@ -94,11 +94,104 @@ const getForm = () => {
           return displayError(errDiv, result);
         }
         errDiv.display = 'none';
-        
+
         const str = JSON.stringify(result);
         localStorage.setItem('token', str);
         setLoggedInClass(true);
-        periodicTokenRenew();
+        window.location = 'http://localhost:3000/';
+      }).catch(console.log);
+  });
+};
+
+const deleteToken = () => {
+  const tokenStr = getToken();
+  const { token } = JSON.parse(tokenStr);
+  localStorage.removeItem('token');
+  setLoggedInClass(false);
+  return request('http://localhost:3000/api/tokens', 'DELETE', {}, { token })
+    .then(({ result, statusCode }) => {
+      if (statusCode !== 200) {
+        console.log(result);
+        return;
+      }
+      window.location = 'http://localhost:3000/';
+    }).catch(console.log);
+};
+
+const getLogoutButton = () => {
+  const logoutButton = document.querySelector("#logout");
+  logoutButton && logoutButton.addEventListener('click', deleteToken);
+};
+
+const checkToken = () => {
+  const tokenStr = getToken();
+  if (tokenStr) {
+    const { expires } = JSON.parse(tokenStr);
+    if (Date.now() > expires) {
+      localStorage.removeItem('token');
+      setLoggedInClass(false);
+    }
+  }
+};
+
+const getEditForm = () => {
+  const editForm = document.querySelector('form#accountEdit');
+  if (editForm) {
+    const { elements } = editForm;
+    const tokenStr = getToken();
+    const { token, phone } = JSON.parse(tokenStr);
+
+    request('http://localhost:3000/api/users', 'GET', {}, { phone }, { token })
+      .then(({ result, statusCode }) => {
+        if (statusCode !== 200) return displayError(errDiv, result);
+        for (const element of elements) {
+          let { name } = element;
+          if (element.type === 'checkbox') {
+            element.checked = true;
+            continue;
+          }
+          element.value = result[name] || '';
+        };
+      }).catch(console.log);
+
+    editForm.addEventListener('submit', (e) => {
+      e.preventDefault();
+      const errDiv = document.querySelector(`#${editForm.id} .formError`);
+      const { elements, action } = editForm;
+      console.dir({ elements, action });
+      const payload = {};
+      for (const element of elements) {
+        let { name, value, type } = element;
+        if (type === 'submit') continue;
+        if (type === 'checkbox') {
+          payload[name] = element.checked;
+          continue;
+        }
+        payload[name] = value;
+      }
+      const tokenStr = getToken();
+      const { token, phone } = JSON.parse(tokenStr);
+      request(action, 'PUT', payload, { phone }, { token })
+        .then(({ result, statusCode }) => {
+          if (statusCode !== 200) {
+            if (statusCode !== 200) return displayError(errDiv, result);
+            errDiv.display = 'none';
+          }
+          window.location = 'http://localhost:3000/';
+        }).catch(console.log);
+    });
+  }
+};
+
+const getDeleteButton = () => {
+  const confirmDeletion = document.querySelector('button#deleteAccount');
+  confirmDeletion && confirmDeletion.addEventListener('click', (e) => {
+    const tokenStr = getToken();
+    const { token, phone } = JSON.parse(tokenStr);
+    request('http://localhost:3000/api/users', 'DELETE', {}, { phone }, { token })
+      .then(({ result, statusCode }) => {
+        if (statusCode !== 200) console.log(result);
+        localStorage.removeItem('token');
         window.location = 'http://localhost:3000/';
       }).catch(console.log);
   });
@@ -108,4 +201,8 @@ window.onload = () => {
   getForm();
   periodicTokenRenew();
   getToken();
+  getLogoutButton();
+  checkToken();
+  getEditForm();
+  getDeleteButton();
 };
